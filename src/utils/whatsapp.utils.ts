ac86141021163ec468c101a69204461efe380fe4
Izzudin Ar Rafiq <izzudin.ar26@gmail.com>
2024-06-17 10:32:58 +0700

    fixing error invalid phone number

--- src/utils/whatsapp.utils.ts
+++ src/utils/whatsapp.utils.ts
@@ -5,5 +5,5 @@ export function transformPhoneNumberToId(phone: string) {
   const isStartWithZero = phone.at(0) == "0";
   if (isStartWithZero) return "62" + phone.slice(1) + suffixPhone;
 
-  return phone + suffixGroup;
+  return phone + suffixPhone;
 }
